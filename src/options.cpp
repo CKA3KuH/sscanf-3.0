eee6e3baee84bc6e70889d832830fe7d8e4440c8
Y_Less <yless42@hotmail.com>
2013-09-01 01:33:59 +0100

    Added options.

--- src/options.cpp
+++ src/options.cpp
@@ -4,7 +4,7 @@
 #include "specifiers/enum_specifier.hpp" // Relies on group_specifiers.
 #include "specifiers/numeric_specifier.hpp"
 #include "specifiers/other_specifiers.hpp"
-#include "specifiers/player_specifier.hpp"
+#include "specifiers/player_specifiers.hpp"
 #include "specifiers/simple_specifiers.hpp"
 #include "specifiers/string_specifier.hpp"
 #include "specifiers/trivial_specifiers.hpp"
